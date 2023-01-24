e927442bc757cb1328f1a5d4466a7b4ec1d0e032
lfpborba <luisfpb@icloud.com>
2023-01-24 04:06:33 -0300

    feat: main function

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,3 +1,25 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return print("Verifique os argumentos", file=sys.stderr)
+
+    inventory_report, file, type = sys.argv
+
+    if file.endswith(".csv"):
+        inventory = InventoryRefactor(CsvImporter)
+    elif file.endswith(".xml"):
+        inventory = InventoryRefactor(XmlImporter)
+    elif file.endswith(".json"):
+        inventory = InventoryRefactor(JsonImporter)
+
+    data = inventory.import_data(file, type)
+
+
+    return print(data, end="")
 # initial commit - plus ultra.
