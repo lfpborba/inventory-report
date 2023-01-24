609519476cd89762db2969149200661a27bdb448
lfpborba <luisfpb@icloud.com>
2023-01-23 23:50:54 -0300

    feat: implementing product test (req_1)

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Apple",
+        "Iphone X",
+        "03/11/17",
+        "00/00/00",
+        "03112017",
+        "Local seco",
+    )
+
+assert product_test.id == 1
+assert product_test.nome_da_empresa == "Apple"
+assert product_test.nome_do_produto == "Iphone X"
+assert product_test.data_de_fabricacao == "03/11/17"
+assert product_test.data_de_validade == "00/00/00"
+assert product_test.numero_de_serie == "03112017"
+assert product_test.instrucoes_de_armazenamento == "Local seco"
