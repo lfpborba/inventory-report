a3634857f4e10c8bf54372178f781cc22a2fbc47
lfpborba <luisfpb@icloud.com>
2023-01-24 00:58:24 -0300

    feat: fixing testing function

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -4,18 +4,18 @@ from inventory_report.inventory.product import Product
 def test_cria_produto():
     product = Product(
         1,
-        "Apple",
         "Iphone X",
+        "Apple",
         "03/11/17",
         "00/00/00",
         "03112017",
-        "Local seco",
-    )
+        "Local seco"
+        )
 
-assert product_test.id == 1
-assert product_test.nome_da_empresa == "Apple"
-assert product_test.nome_do_produto == "Iphone X"
-assert product_test.data_de_fabricacao == "03/11/17"
-assert product_test.data_de_validade == "00/00/00"
-assert product_test.numero_de_serie == "03112017"
-assert product_test.instrucoes_de_armazenamento == "Local seco"
+    assert product.id == 1
+    assert product.nome_do_produto == "Iphone X"
+    assert product.nome_da_empresa == "Apple"
+    assert product.data_de_fabricacao == "03/11/17"
+    assert product.data_de_validade == "00/00/00"
+    assert product.numero_de_serie == "03112017"
+    assert product.instrucoes_de_armazenamento == "Local seco"
