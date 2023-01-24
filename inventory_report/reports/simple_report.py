8b3f53d7d1327075feae05269ccb15153d456dce
lfpborba <luisfpb@icloud.com>
2023-01-24 01:24:14 -0300

    feat: creating a simple report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,24 @@
+from datetime import date
+from collections import Counter
+
 class SimpleReport:
-    pass
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def generate(self, inventory: list):
+
+        production_date = min([item["data_de_fabricação"] for item in inventory])
+
+        date_now = date.today().strftime("%Y-%m-%d")
+
+        validate = min([item["data_de_validade"] for item in inventory
+        if item["data_de_validade"] > date_now])
+
+        more_products, _ = Counter([item["nome_da_empresa"] for item in inventory]).most_common()[0]
+        
+        return (
+            f"Data de fabricação mais antiga: {production_date}"
+            f"Data de validade mais próxima: {validate}"
+            f"Empresa com mais produtos: {more_products}"
+        )
