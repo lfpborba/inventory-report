2af2fbf402cd1ca072b72587bcbafed1e1791b8e
lfpborba <luisfpb@icloud.com>
2023-01-24 03:26:11 -0300

    feat: implementing test_product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    test_product = Product(
+        "18",
+        "World of Warcraft",
+        "Blizzard",
+        "23/11/2004",
+        "11/11/2040",
+        "23112004",
+        "em um SSD",
+    )
+
+    report = test_product.__repr__()
+
+    assert report == (
+        "O produto World of Warcraft fabricado em 23/11/2004 "
+        "por Blizzard com validade até 11/11/2040 "
+        "precisa ser armazenado em um SSD."
+    )
