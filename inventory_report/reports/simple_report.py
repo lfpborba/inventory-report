e74d9a968193b28c74d274280aee045f950d8e75
lfpborba <luisfpb@icloud.com>
2023-01-24 01:49:43 -0300

    feat: fixing simple_report function

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -8,7 +8,7 @@ class SimpleReport:
     @classmethod
     def generate(self, inventory: list):
 
-        production_date = min([item["data_de_fabricação"] for item in inventory])
+        production_date = min([item["data_de_fabricacao"] for item in inventory])
 
         date_now = date.today().strftime("%Y-%m-%d")
 
@@ -18,7 +18,7 @@ class SimpleReport:
         more_products, _ = Counter([item["nome_da_empresa"] for item in inventory]).most_common()[0]
         
         return (
-            f"Data de fabricação mais antiga: {production_date}"
-            f"Data de validade mais próxima: {validate}"
+            f"Data de fabricação mais antiga: {production_date}\n"
+            f"Data de validade mais próxima: {validate}\n"
             f"Empresa com mais produtos: {more_products}"
         )
