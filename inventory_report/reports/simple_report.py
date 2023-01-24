70a4cef907690d22c0ddf2ef1e9736776b14605b
lfpborba <luisfpb@icloud.com>
2023-01-24 03:48:12 -0300

    fix: Linter Eternal Saga pt.1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,22 +1,27 @@
 from datetime import date
 from collections import Counter
 
+
 class SimpleReport:
     def __init__(self):
         pass
-    
+
     @staticmethod
     def generate(inventory: list):
 
-        production_date = min([item["data_de_fabricacao"] for item in inventory])
+        production_date = min(
+            [item["data_de_fabricacao"] for item in inventory]
+        )
 
         date_now = date.today().strftime("%Y-%m-%d")
 
         validate = min([item["data_de_validade"] for item in inventory
-        if item["data_de_validade"] > date_now])
+                       if item["data_de_validade"] > date_now])
 
-        more_products, _ = Counter([item["nome_da_empresa"] for item in inventory]).most_common()[0]
-        
+        more_products, _ = Counter(
+            [item["nome_da_empresa"] for item in inventory]
+        ).most_common()[0]
+
         return (
             f"Data de fabricação mais antiga: {production_date}\n"
             f"Data de validade mais próxima: {validate}\n"
